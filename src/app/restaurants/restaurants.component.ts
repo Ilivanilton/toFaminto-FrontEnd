f66bf85fcae481a1a0ebc60c1676d9199a931e0a
ilivanilton <ilivanilton@gmail.com>
2019-10-14 17:50:43 -0300

    implementado Comopente Restaurants

---
+++ src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tf-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
