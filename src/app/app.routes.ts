c4e78938e29c2465f7841c94a569d4ceaada259e
ilivanilton <ilivanilton@gmail.com>
2019-10-14 17:49:42 -0300

    implementado listagem de restaurantes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
 
 
 
 export const ROUTE: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'sobre', component: AboutComponent}
+    {path: 'sobre', component: AboutComponent},
+    {path: 'restaurantes', component: RestaurantsComponent}
 ]
