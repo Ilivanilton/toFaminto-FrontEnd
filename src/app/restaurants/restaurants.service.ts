7644fe327387ab87539b97abc1e8d2afb789f1fe
ilivanilton <ilivanilton@gmail.com>
2019-10-14 22:20:22 -0300

    tratamento de erros

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,8 @@ import { Http } from "@angular/http";
 import { TF_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
 import "rxjs/add/operator/map"
+import "rxjs/add/operator/catch"
+import { ErrorHandler } from "app/app.error-handler";
 
 
 @Injectable()
@@ -16,6 +18,7 @@ export class RestaurantsService {
     restaurants(): Observable<Restaurant[]>{
         return this.http.get(`${TF_API}/restaurants`)
             .map(response => response.json())
+            .catch(ErrorHandler.HandlerError)
     }
 
 }
