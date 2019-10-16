c378d813fa58732d3409b7749d0fa7e365425463
ilivanilton <ilivanilton@gmail.com>
2019-10-16 18:11:05 -0300

    criado itens do menu

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { MenuItem } from './menu-item.model';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'tf-menu-item',
   templateUrl: './menu-item.component.html'
 })
 export class MenuItemComponent implements OnInit {
+
+@Input()  menuItem: MenuItem
 
   constructor() { }
 
